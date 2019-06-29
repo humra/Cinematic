e7138dede3740e1b38b7bb9fd5ab518b9e3e8041
Humra <matija.huremovic@gmail.com>
2019-06-29 13:19:39 +0200

    Created a separate repository for the cinematic

---
+++ Cinematic/Assets/Scripts/Runner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Runner : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Rigidbody rb;
+
+    public Transform target;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        agent.SetDestination(target.position);
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+    }
+}
